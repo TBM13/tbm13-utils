4e0815162170f0d16b7f143da2a78850fee3e4bd
TBM13 <32852493+TBM13@users.noreply.github.com>
2023-06-30 12:03:06 -0300

    Encoding: Implement url_decode() & url_encode()

--- src/tbm13_utils/encoding.py
+++ src/tbm13_utils/encoding.py
@@ -1,6 +1,8 @@
 import base64
+import urllib.parse
 __all__ = [
-    'base64_decode', 'base64_encode'
+    'base64_decode', 'base64_encode',
+    'url_decode', 'url_encode'
 ]
 
 def base64_decode(s: str) -> str:
@@ -8,9 +10,15 @@ def base64_decode(s: str) -> str:
 
     data_bytes = base64.b64decode(s.encode('utf8'))
     return data_bytes.decode('utf8')
-
 def base64_encode(s: str) -> str:
     """Encodes a string with Base64."""
 
     data_bytes = base64.b64encode(s.encode('utf8'))
     return data_bytes.decode('utf8')
+
+def url_decode(url: str) -> str:
+    """Decodes (unquotes) `url`."""
+    return urllib.parse.unquote(url)
+def url_encode(url: str) -> str:
+    """Encodes (quotes) `url`."""
+    return urllib.parse.quote(url)
