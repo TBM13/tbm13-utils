4003f2e6f17e7baf6bcb614612d5734e9e769027
TBM13 <mateotrueba@gmail.com>
2024-10-31 16:12:33 -0300

    environment: Add `popen_as_root` & root support in `run_and_print_output`

--- src/tbm13_utils/environment.py
+++ src/tbm13_utils/environment.py
@@ -6,7 +6,8 @@ import time
 from typing import Callable
 
 __all__ = [
-    'IN_COLAB', 'tool_exists', 'get_unique_file', 'run_as_root', 'run_and_print_output'
+    'IN_COLAB', 'tool_exists', 'get_unique_file',
+    'run_as_root', 'popen_as_root', 'run_and_print_output'
 ]
 
 # Check if we are being executed on Google Colab
@@ -55,17 +56,28 @@ def get_unique_file(file_path: str) -> str:
 def run_as_root(args, **kwargs) -> subprocess.CompletedProcess:
     """Calls `subprocess.run` with `args` and `kwargs`.
     
-    If the current user is not root, prepend the command with `sudo`.
+    If the current user isn't root, prepend the command with `sudo`.
     """
-
     if os.geteuid() != 0:
         args = ['sudo'] + list(args)
 
     return subprocess.run(args, **kwargs)
 
-def run_and_print_output(print_func: Callable[[str], None],
-                         *args, **kwargs):
+def popen_as_root(args, **kwargs) -> subprocess.Popen:
     """Calls `subprocess.Popen` with `args` and `kwargs`.
+    
+    If the current user isn't root, prepend the command with `sudo`.
+    """
+    if os.geteuid() != 0:
+        args = ['sudo'] + list(args)
+
+    return subprocess.Popen(args, **kwargs)
+
+def run_and_print_output(print_func: Callable[[str], None],
+                         args, root: bool = False,
+                         **kwargs):
+    """Calls `subprocess.Popen` or `popen_as_root` if `root`
+    is `True`, with `args` and `kwargs`.
     
     Pipes both stdout and stderr, and every time the process writes
     a line to stdout, calls `print_func` with it.
@@ -76,7 +88,10 @@ def run_and_print_output(print_func: Callable[[str], None],
     kwargs.setdefault('encoding', 'utf8')
 
     try:
-        proc = subprocess.Popen(*args, **kwargs)
+        if root:
+            proc = popen_as_root(args, **kwargs)
+        else:
+            proc = subprocess.Popen(args, **kwargs)
         while 1:
             line = proc.stdout.readline()
             if not line: 
