b2bb5ae2d71221e8b11679a027e70c8de389d67a
TBM13 <mateotrueba@gmail.com>
2025-01-25 03:45:50 -0300

    settings: Reimplement with `SerializableFile`

--- src/tbm13_utils/settings.py
+++ src/tbm13_utils/settings.py
@@ -1,111 +1,50 @@
-import os
+from typing import Type, TypeVar
 
-from .display import *
+from .flow import *
+from .encoding import *
 __all__ = [
-    'settings_file', 'Setting'
+    'Setting'
 ]
 
-settings_file = 'config'
+class RawSetting(Serializable):
+    def __init__(self, key: str, value: str):
+        self.key = key
+        self.value = value
 
-def _init():
-    """Creates `settings_file` if it doesn't exist."""
+    @classmethod
+    def _create_empty(cls):
+        return cls('', None)
 
-    if not os.path.isfile(settings_file):
-        open(settings_file, 'a').close()
+_settings = SerializableFile('config.json', RawSetting, 'key')
 
-def _read_setting(key: str) -> str | None:
-    """Reads the value of `key` from `settings_file` and returns it.
-
-    If the setting is not set, returns `None`.
-    """
-    _init()
-    with open(settings_file) as file:
-        for line in file:
-            if line.startswith(f'{key}='):
-                return line.rstrip()[(len(key) + 1):]
-
-    return None
-
-def _write_setting(key: str, value: str):
-    """Writes `key` and `value` to `settings_file`."""
-
-    _init()
-    with open(settings_file, 'r+') as file:
-        data = file.read()
-        for line in data.split('\n'):
-            if line.startswith(f'{key}='):
-                # TODO: Use a better method than this
-                data = data.replace(line, f'{key}={value}')
-
-                # Reset pointer and write
-                file.seek(0)
-                file.truncate(0)
-                file.write(data)
-                return
-        
-        # Key not found, so add it
-        file.write(f'{key}={value}\n')
-
-class Setting():
-    def __init__(self, key: str, value_type, default_value=None):
+T = TypeVar('T')
+class Setting:
+    def __init__(self, key: str, value_type: Type[T], default_value: T|None = None):
         self.key = key
         self.value_type = value_type
         self.default_value = default_value
 
-    def read(self):
-        """Reads the value of the setting, parses and returns it.
-        
-        If the value can't be parsed, prints error
-        and returns `default_value` or `None`.
+    @property
+    def value(self) -> T|None:
+        """The value of the setting.
+        Returns `default_value` if not set.
 
-        If the setting is not set and `default_value` isn't `None`,
-        sets the setting to `default_value` and returns it.
-
-        Otherwise, returns `None`.
+        Must always be of type `value_type`. Setting this
+        to `None` will delete the setting.
         """
+        if _settings.contains_key(self.key):
+           return self.value_type(_settings[self.key].value)
 
-        value = _read_setting(self.key)
+        return self.default_value
+    
+    @value.setter
+    def value(self, value: T|None):
         if value is None:
-            if self.default_value is not None:
-                self.write(self.default_value)
-
-            return self.default_value
-
-        try:
-            if self.value_type is bool:
-                value = value.lower()
-                if value == 'true':
-                    value = True
-                elif value == 'false':
-                    value = False
-                else: 
-                    raise ValueError('Not a boolean')
-            elif self.value_type is int:
-                value = int(value)
-            elif self.value_type is float:
-                value = float(value)
-            elif self.value_type is str:
-                pass
-            else:
-                raise ValueError(f'Unknown value type "{self.value_type}"')
-        except ValueError:
-            error(f'[Setting "{self.key}"] Invalid value "{self.value_type}". ' +
-                  f'Returning default value "{self.default_value}"')
-
-            value = self.default_value
-
-        return value
-
-    def write(self, value):
-        """Sets the setting's value to `value`.
-        
-        Returns `True` if successful.
-        """
+            if _settings.contains_key(self.key):
+                _settings.remove_key(self.key)
+            return
 
         if not type(value) is self.value_type:
-            error(f'[Setting "{self.key}"] New value "{value}" ("{type(value)}") ' +
-                  f'doesn\'t match the value type "{self.value_type}"')
-            return False
+            raise AbortInterrupt(f'Setting "{self.key}"\'s value isn\'t of type "{self.value_type}"', value)
 
-        _write_setting(self.key, value)
-        return True
+        _settings[self.key] = RawSetting(self.key, str(value))
