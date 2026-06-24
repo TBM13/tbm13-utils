e1bb5abe396ce35fcc6d3b40d1b64ad37ac3dfab
TBM13 <mateotrueba@gmail.com>
2026-06-23 23:31:16 -0300

    environment: Add `open_unique_file`, improve `run_and_print_output` & refactor

--- src/tbm13_utils/environment.py
+++ src/tbm13_utils/environment.py
@@ -1,122 +1,183 @@
 import os
+import shutil
 import signal
 import subprocess
 import time
+from collections.abc import Callable
+from pathlib import Path
 from subprocess import CompletedProcess, Popen
-from typing import Any, Callable
+from typing import IO, Any, BinaryIO, Literal, TextIO, overload
 
 __all__ = [
-    'get_terminal_columns', 'get_unique_file',
-    'run_as_root', 'popen_as_root', 'run_and_print_output'
+    "get_terminal_columns",
+    "get_unique_path",
+    "open_unique_file",
+    "popen_as_root",
+    "run_and_print_output",
+    "run_as_root",
 ]
 
+
 def get_terminal_columns(fallback: int) -> int:
-    """Returns the number of columns in the terminal.
+    """Returns the number of columns in the terminal or `fallback`."""
+    return shutil.get_terminal_size(fallback=(fallback, 20)).columns
 
-    If something goes wrong, returns `fallback`.
-    """
-    try:
-        return os.get_terminal_size().columns
-    except Exception:
-        return fallback
 
-def get_unique_file(path: str) -> str:
-    """Generates an unique path based on `path` or
-    returns it as-is if it does not exist.
+def get_unique_path(path: str | Path) -> Path:
+    """Returns a unique path based on `path`.
+
+    It is suggested to use `open_unique_file` when possible
+    to avoid TOCTOU issues.
     """
-    path, extension = os.path.splitext(path)
-    unique_path = path + extension
-    i = 1
-    while os.path.exists(unique_path):
-        unique_path = f"{path}_{i}{extension}"
+    path = Path(path)
+    stem, suffix, parent = path.stem, path.suffix, path.parent
+
+    i = 0
+    while True:
+        current_path = path if i == 0 else parent / f"{stem}_{i}{suffix}"
+        if not current_path.exists():
+            return current_path
         i += 1
-    
-    return unique_path
+
+
+@overload
+def open_unique_file(
+    path: str | Path,
+    text: Literal[True] = True,
+    buffering: int = -1,
+    encoding: str | None = None,
+    errors: str | None = None,
+    newline: str | None = None,
+) -> tuple[TextIO, Path]: ...
+@overload
+def open_unique_file(
+    path: str | Path,
+    text: Literal[False],
+    buffering: int = -1,
+) -> tuple[BinaryIO, Path]: ...
+def open_unique_file(
+    path: str | Path,
+    text: bool = True,
+    buffering: int = -1,
+    encoding: str | None = None,
+    errors: str | None = None,
+    newline: str | None = None,
+) -> tuple[IO[str] | IO[bytes], Path]:
+    """Atomically creates and opens a file with a unique name based on `path`.
+
+    The file is opened using the exclusive 'x' mode (text) or 'xb' (binary)
+    depending on `text`.
+
+    Returns a tuple containing the created file's handle and its path.
+    """
+    path = Path(path)
+    stem, suffix, parent = path.stem, path.suffix, path.parent
+
+    mode = "x" if text else "xb"
+    i = 0
+    while True:
+        current_path = path if i == 0 else parent / f"{stem}_{i}{suffix}"
+
+        try:
+            # Atomically check and open the file
+            file_handle = current_path.open(
+                mode=mode,
+                buffering=buffering,
+                encoding=encoding,
+                errors=errors,
+                newline=newline,
+            )
+
+            return (file_handle, current_path)
+        except FileExistsError:
+            i += 1
+
+        run_as_root(["asd"], text=False)
+
 
 def run_as_root(args: list[str], **kwargs: Any) -> CompletedProcess[Any]:
     """Calls `subprocess.run` with `args` and `kwargs`.
-    
+
     If the current user isn't root, prepend the command with `sudo`.
     """
-    if os.geteuid() != 0:                   # type: ignore
-        args = ['sudo'] + args
+    if os.geteuid() != 0:  # type: ignore
+        args = ["sudo", *args]
 
-    return subprocess.run(args, **kwargs)   # type: ignore
+    return subprocess.run(args, **kwargs)  # type: ignore  # noqa: PLW1510
+
 
 def popen_as_root(args: list[str], **kwargs: Any) -> Popen[Any]:
     """Calls `subprocess.Popen` with `args` and `kwargs`.
-    
+
     If the current user isn't root, prepend the command with `sudo`.
     """
-    if os.geteuid() != 0:               # type: ignore
-        args = ['sudo'] + args
+    if os.geteuid() != 0:  # type: ignore
+        args = ["sudo", *args]
 
     return Popen(args, **kwargs)
 
-def run_and_print_output(print_func: Callable[[str], None],
-                         args: list[str], root: bool = False,
-                         send_interrupt: bool = False,
-                         **kwargs: Any) -> Popen[Any]:
-    """Calls `subprocess.Popen` or `popen_as_root` if `root`
-    is `True`, with `args` and `kwargs`.
 
-    Pipes both stdout and stderr, and every time the process writes
-    a line to stdout, calls `print_func` with it.
+def run_and_print_output(
+    print_func: Callable[[str], None],
+    args: list[str],
+    root: bool = False,
+    send_interrupt: bool = False,
+    **kwargs: Any,
+) -> Popen[str]:
+    """Calls `subprocess.Popen` with the given args and kwargs.
 
-    * If `send_interrupt` is `True`, sends a SIGINT (or CTRL_BREAK_EVENT on Windows)
+    Pipes both stdout and stderr, calling `print_func` for each
+    line the process outputs.
+
+    * When `send_interrupt` is `True`, sends SIGINT (or CTRL_BREAK_EVENT on Windows)
     to the process when a KeyboardInterrupt occurs. The user will be forced
     to wait for the process to exit.
     """
-    kwargs['stdout'] = subprocess.PIPE
-    kwargs['stderr'] = subprocess.PIPE
-    kwargs.setdefault('encoding', 'utf8')
+    kwargs["stdout"] = subprocess.PIPE
+    kwargs["stderr"] = subprocess.STDOUT
+    kwargs.setdefault("encoding", "utf8")
 
     # On Windows, SIGINT is not supported. We need to create the
     # process in a new process group and send CTRL_BREAK_EVENT to it
-    if send_interrupt and os.name == 'nt':
-        kwargs.setdefault('creationflags', subprocess.CREATE_NEW_PROCESS_GROUP)
-        kwargs['creationflags'] |= subprocess.CREATE_NEW_PROCESS_GROUP
+    if send_interrupt and os.name == "nt":
+        kwargs["creationflags"] = (
+            kwargs.get("creationflags", 0) | subprocess.CREATE_NEW_PROCESS_GROUP
+        )
 
-    if root:
-        proc = popen_as_root(args, **kwargs)
-    else:
-        proc = subprocess.Popen(args, **kwargs)
+    proc = popen_as_root(args, **kwargs) if root else subprocess.Popen(args, **kwargs)
 
-    def read_loop(interrupt_sent: bool):
-        try:
-            for line in proc.stdout:  # type: ignore
-                print_func(line)
-
-            if interrupt_sent:
-                # Wait for the process to gracefully exit
-                proc.wait()
-        except KeyboardInterrupt:
-            if interrupt_sent:
-                # Force user to wait for the process to
-                # gracefully handle the interrupt and exit
-                read_loop(True)
-                return
-            
-            # Send interrupt and wait for process to gracefully exit
-            if send_interrupt:
-                if os.name == 'nt':
-                    os.kill(proc.pid, signal.CTRL_BREAK_EVENT)
-                else:
-                    proc.send_signal(signal.SIGINT)
-
-                read_loop(True)
-                proc.wait()
-            
-            raise KeyboardInterrupt
-
+    interrupt_sent = False
     try:
-        read_loop(False)
-        proc.wait()
+        while True:
+            try:
+                if proc.stdout and not proc.stdout.closed:
+                    for line in proc.stdout:
+                        print_func(line)
+
+                # EOF, wait for the process to exit
+                proc.wait()
+                break
+            except KeyboardInterrupt:
+                if not send_interrupt:
+                    # Hard cancellation
+                    time.sleep(0.1)  # Prevent terminal EOF issues
+                    proc.terminate()
+                    proc.wait()
+                    raise
+
+                if not interrupt_sent:
+                    if os.name == "nt":
+                        os.kill(proc.pid, signal.CTRL_BREAK_EVENT)
+                    else:
+                        proc.send_signal(signal.SIGINT)
+                    interrupt_sent = True
+                else:
+                    # Force user to wait for the process to exit
+                    continue
+
         return proc
-    except KeyboardInterrupt:
-        # Wait a little in order to prevent EOFError
-        # when user presses CTRL + C
-        time.sleep(0.1)
-        raise KeyboardInterrupt
-    finally:
+    except Exception:
+        # Clean up process if any unexpected exception occurs
         proc.terminate()
+        proc.wait()
+        raise
--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,42 +1,78 @@
-import unittest
+import sys
+from pathlib import Path
 from unittest.mock import MagicMock, patch
 
-from tbm13_utils.environment import *
+import pytest
+
+from tbm13_utils.environment import (
+    get_unique_path,
+    open_unique_file,
+    run_and_print_output,
+)
 
 
-class TestEnvironment(unittest.TestCase):
+@patch("pathlib.Path.exists")
+def test_get_unique_path(mock_exists: MagicMock):
+    def do_test(path: str, conflicts: int, expected: str):
+        mock_exists.reset_mock()
+        mock_exists.side_effect = [True] * conflicts + [False]
+        result = get_unique_path(path)
+        assert result == Path(expected)
+        assert mock_exists.call_count == conflicts + 1
 
-    @patch('os.path.exists')
-    def test_get_unique_file(self, mock_exists: MagicMock):
-        def do_test(path: str, conflicts: int, expected: str):
-            mock_exists.reset_mock()
-            mock_exists.side_effect = [True] * conflicts + [False]
-            result = get_unique_file(path)
-            self.assertEqual(result, expected)
-            self.assertEqual(mock_exists.call_count, conflicts + 1)
-
-        # No conflicts (file does not exist)
-        do_test('asd', 0, 'asd')
-        do_test('test.txt', 0, 'test.txt')
-        do_test('.gitignore', 0, '.gitignore')
-        do_test('./test', 0, './test')
-        do_test('./.gitignore', 0, './.gitignore')
-        do_test('/asd/test/abc', 0, '/asd/test/abc')
-        # One conflict
-        do_test('asd', 1, 'asd_1')
-        do_test('test.txt', 1, 'test_1.txt')
-        do_test('.gitignore', 1, '.gitignore_1')
-        do_test('./test', 1, './test_1')
-        do_test('./.gitignore', 1, './.gitignore_1')
-        do_test('/asd/test/abc.txt', 1, '/asd/test/abc_1.txt')
-        # Multiple conflicts
-        do_test('asd', 3, 'asd_3')
-        do_test('test.txt', 3, 'test_3.txt')
-        do_test('.gitignore', 3, '.gitignore_3')
-        do_test('./test', 3, './test_3')
-        do_test('./.gitignore', 3, './.gitignore_3')
-        do_test('/asd/test/abc.txt', 3, '/asd/test/abc_3.txt')
+    # No conflicts (file does not exist)
+    do_test("asd", 0, "asd")
+    do_test("test.txt", 0, "test.txt")
+    do_test(".gitignore", 0, ".gitignore")
+    do_test("./test", 0, "./test")
+    do_test("./.gitignore", 0, "./.gitignore")
+    do_test("/asd/test/abc", 0, "/asd/test/abc")
+    # One conflict
+    do_test("asd", 1, "asd_1")
+    do_test("test.txt", 1, "test_1.txt")
+    do_test(".gitignore", 1, ".gitignore_1")
+    do_test("./test", 1, "./test_1")
+    do_test("./.gitignore", 1, "./.gitignore_1")
+    do_test("/asd/test/abc.txt", 1, "/asd/test/abc_1.txt")
+    # Multiple conflicts
+    do_test("asd", 3, "asd_3")
+    do_test("test.txt", 3, "test_3.txt")
+    do_test(".gitignore", 3, ".gitignore_3")
+    do_test("./test", 3, "./test_3")
+    do_test("./.gitignore", 3, "./.gitignore_3")
+    do_test("/asd/test/abc.txt", 3, "/asd/test/abc_3.txt")
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_open_unique_file(tmp_path: Path):
+    def write_file(path: str, expected_path: str):
+        file_handle, final_path = open_unique_file(tmp_path / path)
+        with file_handle:
+            file_handle.write("test")
+
+        assert final_path.exists()
+        assert final_path == tmp_path / expected_path
+
+    # No conflicts (file does not exist)
+    write_file("asd", "asd")
+    write_file("test.txt", "test.txt")
+    write_file(".gitignore", ".gitignore")
+    write_file("./test", "./test")
+    # One conflict
+    write_file("asd", "asd_1")
+    write_file("test.txt", "test_1.txt")
+    write_file(".gitignore", ".gitignore_1")
+    write_file("./test", "./test_1")
+    # Two conflicts
+    write_file("asd", "asd_2")
+    write_file("test.txt", "test_2.txt")
+    write_file(".gitignore", ".gitignore_2")
+    write_file("./test", "./test_2")
+
+
+def test_run_and_print_output():
+    # Very simple test just to ensure the function runs and captures output correctly.
+    captured_lines: list[str] = []
+    args = [sys.executable, "-c", "import sys; sys.stdout.write('hello\\nworld\\n')"]
+    run_and_print_output(captured_lines.append, args)
+
+    assert captured_lines == ["hello\n", "world\n"]
